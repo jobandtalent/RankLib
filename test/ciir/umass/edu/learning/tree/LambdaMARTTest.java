60cabd8645b7d4a62777174d14998eab55d67917
Oscar Huarte <oscarhuarte@MacBook-Pro-de-Oscar.local>
2016-03-28 16:38:46 +0200

    added integration test with RankerFactory

--- test/ciir/umass/edu/learning/tree/LambdaMARTTest.java
+++ test/ciir/umass/edu/learning/tree/LambdaMARTTest.java
@@ -1,20 +1,33 @@
 package ciir.umass.edu.learning.tree;
 
 
+import ciir.umass.edu.learning.Ranker;
+import ciir.umass.edu.learning.RankerFactory;
 import ciir.umass.edu.utilities.FileUtils;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class LambdaMARTTest {
+
+    private static int[] FEATURES = new int[]{2, 5, 6, 7, 8, 9};
+
+    @Test
+    public void testLoadFromFactory() {
+        RankerFactory factoryRanker = new RankerFactory();
+        Ranker ranker = factoryRanker.loadRankerFromFile("resources/lambdamart.model");
+
+        assertEquals(ranker.name(), "LambdaMART");
+        assertArrayEquals(ranker.getFeatures(), FEATURES);
+    }
 
     @Test
     public void testLoadFromString() {
         LambdaMART mart = new LambdaMART();
         mart.loadFromString(FileUtils.read("resources/lambdamart.model", "UTF-8"));
 
-        int[] features = mart.ensemble.getFeatures();
-        assertArrayEquals(features, new int[]{2, 5, 6, 7, 8, 9});
-
+        assertEquals(mart.name(), "LambdaMART");
+        assertArrayEquals(mart.ensemble.getFeatures(), FEATURES);
     }
 }
